856e93ecdb6b7bd6b6b2974b8d47062078c61298
komukomo <komukomo@users.noreply.github.com>
2016-04-28 23:06:37 +0900

    Update

--- react/src/es2015/application.js
+++ react/src/es2015/application.js
@@ -1,24 +1,54 @@
 import hello from "exports?hello!../hoge/hoge";
 import React from "react";
+import marked from "marked";
 import ReactDOM from "react-dom";
 
+let data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 const CommentBox = React.createClass({
-  render: () => {
+  render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
   }
 });
 
+const Comment = React.createClass({
+  rawMarkup: function() {
+    const rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return { __html: rawMarkup };
+  },
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+});
+
 const CommentList = React.createClass({
-  render: () => {
+  render: function() {
+    let commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={Comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
       <div className="commentList">
-        commentList...
+        {commentNodes}
       </div>
     );
   }
@@ -34,8 +64,9 @@ const CommentForm = React.createClass({
   }
 });
 
+
 ReactDOM.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('example')
 );
 
