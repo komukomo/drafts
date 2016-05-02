64f0dd36f278d437cdc148d15c1028da2cadb717
komukomo <komukomo@users.noreply.github.com>
2016-05-02 18:01:17 +0900

    init server

--- react/server.js
+++ react/server.js
@@ -1,20 +1,27 @@
-import path from 'path'
-import Express from 'express'
-import React from 'react'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import counterApp from './reducers'
-import App from './containers/App'
+var webpack = require('webpack')
+var webpackDevMiddleware = require('webpack-dev-middleware')
+var webpackHotMiddleware = require('webpack-hot-middleware')
+var config = require('./webpack.config')
 
-const app = Express()
-const port = 3000
+var app = new (require('express'))()
+var port = 3000
 
-app.use(handleRender)
+var compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
 
-function handleRender(req, res) {
-}
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html')
+})
 
-function renderFullPage(html, initialState) {
-}
+app.get("/data.json", function(req, res) {
+  res.sendFile(__dirname + '/dist/sample-data.json')
+})
 
-app.listen(port)
+app.listen(port, function(error) {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+  }
+})
