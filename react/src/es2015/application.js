58560770d997166ac76e8ff01dbaaa3948cad89a
komukomo <komukomo@users.noreply.github.com>
2016-05-03 22:48:53 +0900

    Add Calendar

--- react/src/es2015/application.js
+++ react/src/es2015/application.js
@@ -32,6 +32,7 @@ const CommentBox = React.createClass({
         <h1>Comments</h1>
         <CommentList data={this.state.data} />
         <CommentForm />
+        <Calendar />
       </div>
     );
   }
@@ -80,6 +81,89 @@ const CommentForm = React.createClass({
     );
   }
 });
+
+const weeks = [
+  [1, 2, 3, 4, 5, 6, 7],
+  [8, 9, 10, 11, 12, 13, 14],
+  [15, 16, 17, 18, 19, 20, 21],
+  [22, 23, 24, 25, 26, 27, 28],
+  [29, 30, 31]
+];
+
+
+const CalendarCell = React.createClass({
+  getInitialState: function() {
+    return {order: ""};
+  },
+  handleChange: function(e) {
+    const value = e.currentTarget.value;
+    if (e.currentTarget.value === this.state.order) {
+      this.setState({order: null}); // cancel
+    } else {
+      this.setState({order: value});
+    }
+  },
+  render: function() {
+    const disabled = this.props.date < this.props.today;
+    return (
+      <div>
+        {this.props.date} {disabled}
+        <label>
+          <input
+            name={this.props.date}
+            type="radio"
+            onChange={this.handleChange}
+            value="180"
+            checked={this.state.order === "180"}
+            disabled={disabled}
+          />
+          {disabled}g
+        </label>
+        <label>
+          <input name={this.props.date}
+            type="radio"
+            onChange={this.handleChange}
+            value="240"
+            checked={this.state.order === "240"}
+            disabled={disabled}
+          />
+          240g
+        </label>
+      </div>
+    )
+  }
+});
+
+const Calendar = React.createClass({
+  render: function() {
+    let that = this;
+    let today = (new Date()).getDate();
+    return (
+      <div>
+        <table>
+          <tbody>
+            {weeks.map((week) => {
+              return (
+                <tr>
+                  {
+                    week.map((date) => {
+                      return (
+                        <td>
+                          <CalendarCell today={today} date={date} />
+                        </td>
+                      )
+                    })
+                  }
+                </tr>
+              )}
+            )}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+});
+
 
 
 ReactDOM.render(
